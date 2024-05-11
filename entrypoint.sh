e65b5c22fa98b75f244c6d55a522b060c4656e90
cyb3rgh05t <flammang.yves@gmail.com>
2024-05-11 12:24:41 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 function cleanup {
-    umount /merged
 	umount /unionfs
 }
 
-mergerfs -o rw,use_ino,nonempty,allow_other,statfs_ignore=nc,func.getattr=newest,category.action=all,category.create=ff,cache.files=auto-full,dropcacheonclose=true,fsname=mergerfs /disks/*: /merged
+mergerfs -o rw,use_ino,nonempty,allow_other,statfs_ignore=nc,func.getattr=newest,category.action=all,category.create=ff,cache.files=auto-full,dropcacheonclose=true,fsname=mergerfs /disks/*: /unionfs
 
 
 trap cleanup EXIT INT
